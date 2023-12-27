e6c1fe9130363a236b24916f81c9dd9b5cd45a3e
Kendal Katherine <kendal.katherine3@gmail.com>
2023-12-27 20:33:12 -0300

    Inicio do Projeto

---
+++ src/main/java/com/generation/quitanda/QuitandaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.quitanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuitandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuitandaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/quitanda/model/Produtos.java
@@ -0,0 +1,5 @@
+package com.generation.quitanda.model;
+
+public class Produtos {
+
+}
